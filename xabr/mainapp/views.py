475d255a6151290a75a497ff97322c13cbbdcca7
Dmirty <dmitry.ml@mail.ru>
2021-01-18 00:25:17 +0300

    Удалил лишние на главной

--- xabr/mainapp/views.py
+++ xabr/mainapp/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        # 'page_title': 'главная'.title(),  # bad idea
         'page_title': 'главная',
     }
     return render(request, 'mainapp/index.html', context)
