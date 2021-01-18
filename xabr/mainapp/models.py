b51dfc046b4f2a20be592b8e54b4afbe47d20b4e
alekskositsyn <61062623+alekskositsyn@users.noreply.github.com>
2021-01-18 21:46:42 +0300

    change model category

--- xabr/mainapp/models.py
+++ xabr/mainapp/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 
 
-class XabrCategory(models.Model):
-    name = models.CharField('название категории', max_length=64)
-    description = models.TextField('описание категории', blank=True)
-    is_active = models.BooleanField(verbose_name='активна', default=True)
+# python manage.py migrate
+# python manage.py makemigrations
+# python manage.py migrate
+# Создать суперпользователя через консоль: python manage.py createsuperuser / django / geekbrains
 
 
+class Category(models.Model):
+    name = models.CharField(verbose_name='название категории', max_length=64, default=1, unique=True)
+    description = models.TextField(verbose_name='описание категории', blank=True)
 
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='название статьи', max_length=128)
+    description = models.TextField(verbose_name='описание статьи', blank=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.category.name})"
