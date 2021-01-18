204c827ea4e3eacd9c7aea85c90cdb5854c8c47a
Solntseva24 <w-j-olga24@mail.ru>
2021-01-18 20:06:17 +0300

    создание модели ХабрКатегория

--- xabr/mainapp/models.py
+++ xabr/mainapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class XabrCategory(models.Model):
+    name = models.CharField('название категории', max_length=64)
+    description = models.TextField('описание категории', blank=True)
+    is_active = models.BooleanField(verbose_name='активна', default=True)
+
+
+
