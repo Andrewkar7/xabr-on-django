2169afa455631be4c1c19d0eca852599f33cef4d
Solntseva24 <w-j-olga24@mail.ru>
2021-01-19 21:02:59 +0300

    добавлена страница Дизайн, к ней view  и url,прописан путь в меню к странице Дизайн и Главная, подключен файл css

--- xabr/mainapp/views.py
+++ xabr/mainapp/views.py
@@ -7,3 +7,25 @@ def index(request):
         'page_title': 'главная',
     }
     return render(request, 'mainapp/index.html', context)
+
+
+
+def post(request, pk):
+    context = {
+        'page_title': 'хабр',
+    }
+    return render(request, 'mainapp/post.html', context)
+
+
+def help(request):
+    context = {
+        'page_title': 'помощь',
+    }
+    return render(request, 'mainapp/help.html', context)
+
+def design(request):
+    context = {
+        'page_title': 'дизайн',
+    }
+    return render(request, 'mainapp/design.html', context)
+
