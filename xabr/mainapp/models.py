0c40c03dff25e6aea917fb2b0d8aaee4d199ca70
alekskositsyn <61062623+alekskositsyn@users.noreply.github.com>
2021-01-20 11:46:33 +0300

    Пофиксил миграции с полем create_datetime

--- xabr/mainapp/models.py
+++ xabr/mainapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # python manage.py migrate
 # python manage.py makemigrations
 # python manage.py migrate
@@ -18,8 +19,9 @@ class Post(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(verbose_name='название статьи', max_length=128)
     description = models.TextField(verbose_name='описание статьи', blank=True)
-    create_datetime = models.DateTimeField('дата создания', auto_now_add=True)
-
+    create_datetime = models.DateField(verbose_name='дата создания',
+                                       auto_now_add=True,
+                                       blank=True)  # необходимо указывать в [default: timezone.now] >>> timezone.now
 
     def __str__(self):
         return f"{self.name} ({self.category.name})"
