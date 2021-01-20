abfd83c2387e38f7ba261932bab65d7e620103f5
Solntseva24 <w-j-olga24@mail.ru>
2021-01-20 22:31:23 +0300

    попытка сделать вывод по категориям

--- xabr/mainapp/urls.py
+++ xabr/mainapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', mainapp.index, name='index'),
     path('post/<int:pk>/', mainapp.post, name='post'),
     path('help/', mainapp.help, name='help'),
+    path('category_page/<int:pk>/', mainapp.category_page, name='category_page'),
 ]
--- xabr/mainapp/views.py
+++ xabr/mainapp/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Post
 
+def get_menu():
+    print(Category.objects.all())
+    return Category.objects.all()
 
 def index(request):
+
+
     context = {
         'page_title': 'главная',
+        'categories': get_menu(),
     }
     return render(request, 'mainapp/index.html', context)
 
@@ -24,3 +30,18 @@ def help(request):
     }
     return render(request, 'mainapp/help.html', context)
 
+
+def category_page(request, pk):
+
+    if pk == '0':
+        category = {'pk': 0, 'name': 'все'}
+    else:
+        category = get_object_or_404(Category, pk=pk)
+
+
+    context = {
+        'page_title': 'главная',
+        'categories': get_menu(),
+        'category': category,
+    }
+    return render(request, 'mainapp/category_page.html', context)
