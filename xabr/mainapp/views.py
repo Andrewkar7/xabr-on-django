930bb4c29ad5e18668560bca884eb45ff041de13
Solntseva24 <w-j-olga24@mail.ru>
2021-01-21 22:52:16 +0300

    настроила вывод статей по категориям со страницы category_page

--- xabr/mainapp/views.py
+++ xabr/mainapp/views.py
@@ -33,15 +33,20 @@ def help(request):
 
 def category_page(request, pk):
 
+
     categories = Category.objects.all()
+
     if pk == '0':
         category = {'pk': 0, 'name': 'все'}
+        posts = Post.objects.all().order_by('-create_datetime')
     else:
         category = get_object_or_404(Category, pk=pk)
+        posts = category.post_set.order_by('-create_datetime')
 
     context = {
         'page_title': 'главная',
         'categories': categories,
         'category': category,
+        'posts': posts,
     }
     return render(request, 'mainapp/category_page.html', context)
