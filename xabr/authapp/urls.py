a655f1f36a36db9a23e53e05c6ac1538c10f8596
Solntseva24 <w-j-olga24@mail.ru>
2021-01-24 23:37:22 +0300

    добавление приложения authapp

---
+++ xabr/authapp/urls.py
@@ -0,0 +1,13 @@
+import mainapp.views as mainapp
+from django.urls import path
+
+
+app_name = 'mainapp'
+
+urlpatterns = [
+    path('', mainapp.index, name='index'),
+    path('post/<slug:slug>/', mainapp.post, name='post'),
+    path('help/', mainapp.help, name='help'),
+    path('category/<slug:slug>/', mainapp.category_page, name='category_page'),
+]
+
