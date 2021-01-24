e2bc905de63ae822d466508d44de38e036797393
Dmirty <dmitry.ml@mail.ru>
2021-01-24 22:36:21 +0300

    добавлены чпу, топ

--- xabr/mainapp/urls.py
+++ xabr/mainapp/urls.py
@@ -6,7 +6,8 @@ app_name = 'mainapp'
 
 urlpatterns = [
     path('', mainapp.index, name='index'),
-    path('post/<int:pk>/', mainapp.post, name='post'),
+    path('post/<slug:slug>/', mainapp.post, name='post'),
     path('help/', mainapp.help, name='help'),
-    path('category/<int:pk>/page/', mainapp.category_page, name='category_page'),
+    path('category/<slug:slug>/', mainapp.category_page, name='category_page'),
 ]
+
--- xabr/mainapp/views.py
+++ xabr/mainapp/views.py
@@ -16,31 +16,37 @@ def index(request):
 
 def post(request, slug):
     posts = Post.objects.filter(slug=slug)
+    categories = Category.objects.all()
     context = {
         'page_title': 'хабр',
-        'posts': posts
+        'posts': posts,
+        'categories': categories
     }
     return render(request, 'mainapp/post.html', context)
 
 
 def help(request):
+    categories = Category.objects.all()
     context = {
         'page_title': 'помощь',
+        'categories': categories
     }
     return render(request, 'mainapp/help.html', context)
 
 
 def category_page(request, slug):
-
     categories = Category.objects.all()
     if slug == '':
         category = {'slug': '', 'name': 'все'}
+        posts = Post.objects.all().order_by('-create_datetime')
     else:
         category = get_object_or_404(Category, slug=slug)
+        posts = category.post_set.order_by('-create_datetime')
 
     context = {
         'page_title': 'главная',
         'categories': categories,
         'category': category,
+        'posts': posts,
     }
     return render(request, 'mainapp/category_page.html', context)
