a36a0d31fce926e466c70fab2d40d06d60eb7610
Dmirty <dmitry.ml@mail.ru>
2021-02-07 18:02:52 +0300

    подогнал шаблон для удаления одной статьи пользователя и возвратом на страницу список статей

--- xabr/blogapp/views.py
+++ xabr/blogapp/views.py
@@ -44,4 +44,4 @@ class BlogUpdateView(UpdateView):
 class BlogDeleteView(DeleteView):
     model = Post
     template_name = 'post_delete.html'
-    success_url = reverse_lazy('main:index')
+    success_url = reverse_lazy('blog:post_list')
