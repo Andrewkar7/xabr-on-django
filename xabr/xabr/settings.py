194c49a14499e7b2596fe0c65987ce9e46027066
Dmirty <dmitry.ml@mail.ru>
2021-02-14 22:04:37 +0300

    Могу открыть страницу добавления статьи, сходу нашел поле для пользователя и опубликовал статью - исправлено добавил login_required в post/new/

--- xabr/xabr/settings.py
+++ xabr/xabr/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = (
- #   'static',
-#)
+STATICFILES_DIRS = (
+    'static',
+)
 
 AUTH_USER_MODEL = 'authapp.XabrUser'
 
