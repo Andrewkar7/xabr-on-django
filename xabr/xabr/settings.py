593762bf424d94b353a0049cdf82d90a0acff209
Dmirty <dmitry.ml@mail.ru>
2021-02-27 12:35:39 +0300

    документация

--- xabr/xabr/settings.py
+++ xabr/xabr/settings.py
@@ -7,17 +7,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-
 import os
-
-
-import export as export
 from django.conf import settings
 from email_setting import email_settings
-
-
-
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
